eab456e09f7c95037db6f034075902024b38c8f5
Rishabh Bansal <96bansal.rishabh@gmail.com>
2025-09-28 14:48:16 +0530

    for Type Conversion

---
+++ TypeConversion.py
@@ -0,0 +1,34 @@
+# int conversion
+print(int(3))
+print(int(3.5))
+print(int(True))
+print(int('  31  '))
+print('----------------------')
+
+#float conversion
+print(float(3))
+print(float(3.5))
+print(float(True))
+print(float('56'))
+print('----------------------')
+
+#string conversion
+print(str(3))
+print(str(3.5))
+print(str(True))
+print(str('jiji'))
+print('----------------------')
+
+#Boolean conversion
+print(bool(3832))
+print(bool(3832.748347))
+print(bool(0))
+print(bool(0.0000))
+print(bool(' '))
+print(bool(''))
+print(bool('3832'))
+print('----------------------')
+
+#two step conversion
+print(int(float("3.14")))
+print(float(int(.322)))
