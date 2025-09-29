64a725cf0fe65a770e4993d6a3ad017952a395ef
Rishabh Bansal <96bansal.rishabh@gmail.com>
2025-09-29 20:09:00 +0530

    ifElse function added

---
+++ ifElse.py
@@ -0,0 +1,36 @@
+# if else practise code
+n = int(input("Enter a number : "))
+if(n>=0):
+    print(n,"it is a +ve number")   
+else:
+    print(n,"it is a -ve number")
+print("_"*20)
+
+
+if(n==0):
+    print(n,"it is zero")
+elif(n>0):
+    print(n,"it is a +ve number")
+else:
+    print(n,"it is a -ve number")
+print("_"*20)
+
+
+if(n%5 == 0):
+    if(n%3 == 0):
+        print(n,"is divisible by both 5 and 3")
+    else:
+        print(n,"is divisible by 5 only")
+else:
+    print(n,"is not divisible by 5")
+print("_"*20)
+
+if((n%5 == 0) and (n%3==0)):
+    print(n,"second divisible by both 5 and 3")
+elif((n%5 == 0) or (n%3==0)):
+    if(n%5 == 0):
+        print(n,"second divisible by 5")
+    else:
+        print(n,"second divisible by 3")
+else:
+    print(n,"second not divisible")
